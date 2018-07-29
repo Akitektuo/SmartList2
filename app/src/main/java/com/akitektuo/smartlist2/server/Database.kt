00a010e873fce759fcc511f6508ca2ac081facce
Akitektuo <alexcopindean@yahoo.com>
2018-07-29 22:25:34 +0300

    Add tables

--- app/src/main/java/com/akitektuo/smartlist2/server/Database.kt
+++ app/src/main/java/com/akitektuo/smartlist2/server/Database.kt
@@ -24,7 +24,77 @@ class Database {
             val fill: Boolean = false,
             val preferredCurrency: String = "eur",
             val graphColumns: Int = 7,
-            val id: String = "")
+            val id: String = ""
+    )
+
+    data class UserList(
+            val userId: String = "",
+            val listId: String = "",
+            val type: Int = 0,
+            val id: String = ""
+    )
+
+    data class List(
+            val name: String = "",
+            val id: String = ""
+    )
+
+    data class Connection(
+            val fromListId: String = "",
+            val toListId: String = "",
+            val id: String = ""
+    )
+
+    data class Condition(
+            val connectionId: String = "",
+            val categoryId: String = "",
+            val type: Int = 0,
+            val id: String = ""
+    )
+
+    data class ListItem(
+            val listId: String = "",
+            val name: String = "",
+            val price: Double = 0.0,
+            val currency: String = "",
+            val categoryId: String = "",
+            val timestamp: Long = 0,
+            val id: String = ""
+    )
+
+    data class Product(
+            val userId: String = "",
+            val name: String = "",
+            val categoryId: String = "",
+            val id: String = ""
+    )
+
+    data class Prices(
+            val price: Double = 0.0,
+            val currency: String = "",
+            val productId: String = "",
+            val appearances: Long = 0,
+            val id: String = ""
+    )
+
+    data class Category(
+            val name: String = "",
+            val id: String = ""
+    )
+
+    data class UserListExcel(
+            val userId: String = "",
+            val excelId: String = "",
+            val type: Int = 0,
+            val id: String = ""
+    )
+
+    data class Excel(
+            val listId: String = "",
+            val timestamp: Long = 0,
+            val products: Int = 0,
+            val id: String = ""
+    )
 
     fun isUserSignedIn() = auth.currentUser != null
 
